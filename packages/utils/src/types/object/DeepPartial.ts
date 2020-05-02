928c8e75c55e75b6276604bb50793e34a43fbabb
wx-chevalier <wxchevalier925@gmail.com>
2020-05-03 00:31:13 +0800

    feat: update articles or snippets

--- packages/utils/src/types/object/DeepPartial.ts
+++ packages/utils/src/types/object/DeepPartial.ts
@@ -6,7 +6,10 @@ export type DeepPartial<T> = T extends any[]
   : T extends object
   ? DeepPartialObject<T>
   : T;
+
+/* eslint-disable @typescript-eslint/no-empty-interface */
 interface DeepPartialArray<T> extends Array<DeepPartial<T>> {}
+
 type DeepPartialObject<T> = {
   [P in keyof T]+?: DeepPartial<T[P]>;
 };
