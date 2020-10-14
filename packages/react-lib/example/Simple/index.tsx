42274ee97dafbc91ca8465f2567f2b1cef124d9c
wx-chevalier <wxchevalier925@gmail.com>
2020-10-14 11:28:10 +0800

    feat: update articles or snippets

--- packages/react-lib/example/Simple/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import { Exception } from '../../src';
-
-export default function Simple() {
-  return <Exception />;
-}
--- packages/react-lib/src/decorators/exception/Exception.tsx
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-
-export const Exception = ({ ...args }) => <div {...args}>Simple Exception Component</div>;
