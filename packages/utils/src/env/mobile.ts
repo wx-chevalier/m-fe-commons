cba198e88b64ba1db5b2b7f6cc9336abfeb905bd
wx-chevalier <wxchevalier925@gmail.com>
2020-12-14 13:52:29 +0800

    feat: 移除mobile-detect依赖

--- packages/utils/src/env/mobile.ts
+++ packages/utils/src/env/mobile.ts
@@ -1,13 +1,31 @@
-import MobileDetect from 'mobile-detect';
+const mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
 
-/** 是否为移动端 */
-export function isMobile(_ua?: string) {
-  let ua = _ua;
-  if (typeof window !== undefined && !ua) {
-    ua = window.navigator.userAgent;
+const tabletRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk/i;
+
+/** 判断是否为移动端 */
+export function isMobile(
+  opts: { _ua?: string | any; tablet?: boolean; featureDetect?: boolean } = {},
+) {
+  let ua = opts._ua;
+  if (!ua && typeof navigator !== 'undefined') ua = navigator.userAgent;
+  if (ua && ua.headers && typeof ua.headers['user-agent'] === 'string') {
+    ua = ua.headers['user-agent'];
+  }
+  if (typeof ua !== 'string') return false;
+
+  let result = opts.tablet ? tabletRE.test(ua) : mobileRE.test(ua);
+
+  if (
+    !result &&
+    opts.tablet &&
+    opts.featureDetect &&
+    navigator &&
+    navigator.maxTouchPoints > 1 &&
+    ua.indexOf('Macintosh') !== -1 &&
+    ua.indexOf('Safari') !== -1
+  ) {
+    result = true;
   }
 
-  const md = new MobileDetect(window.navigator.userAgent);
-
-  return !!md.mobile();
+  return result;
 }
