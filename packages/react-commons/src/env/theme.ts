10434d9f12f5c45abe192b3a6ed6eec15858a758
wx-chevalier <wxchevalier925@gmail.com>
2021-01-20 17:04:51 +0800

    feat: 升级react-commons

--- packages/react-commons/src/env/theme.ts
+++ packages/react-commons/src/env/theme.ts
@@ -1,4 +1,4 @@
-import generate from '@ant-design/colors/lib/generate';
+import { generate } from '@ant-design/colors';
 import * as client from 'webpack-theme-color-replacer/client';
 
 export const themeClient = {
--- packages/react-commons/src/shim.d.ts
+++ packages/react-commons/src/shim.d.ts
@@ -1,3 +1,2 @@
-declare module '@ant-design/colors/lib/generate';
 declare module 'webpack-theme-color-replacer/client';
 declare module 'dayjs-ext/plugin/timeZone';
