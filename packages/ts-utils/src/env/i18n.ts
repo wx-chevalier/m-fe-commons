36db9fa6c2f3cd0a6987d717dea890a54cda8d6c
wx-chevalier <384924552@qq.com>
2022-10-25 15:45:07 +0800

    feat: update articles or snippets

--- packages/ts-utils/src/env/i18n.ts
+++ packages/ts-utils/src/env/i18n.ts
@@ -1,12 +1,17 @@
-let i18nFormat: (id: string, values?: Record<string, any>) => string;
+export type I18nFormat = (
+  id: string,
+  values?: Record<string, any>,
+  defaultMessage?: string,
+) => string;
 
-export function setI18nFormat(
-  _i: (id: string, values?: Record<string, any>) => string,
-) {
+let i18nFormat: I18nFormat;
+
+export function setI18nFormat(_i: I18nFormat) {
   i18nFormat = _i;
 }
 
 export function getI18nFormat() {
-  const a = (a: string) => a;
+  const a = (a: string) => `${a}`;
+
   return i18nFormat || a;
 }
